42bed52ebec3e1e8d1ff897c733b68be37638a97
Patrick Maciel <patrickmaciel.info@gmail.com>
2016-09-13 16:29:16 -0300

    Sample files

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- Project: Attack of the Cuteness Game
+-- http://MasteringCoronaSDK.com
+-- Version: 1.0
+-- Copyright 2013 J. A. Whye. All Rights Reserved.
+-- "Space Cute" art by Daniel Cook (Lostgarden.com) 
+
+-- housekeeping stuff
+
+display.setStatusBar(display.HiddenStatusBar)
+
+local centerX = display.contentCenterX
+local centerY = display.contentCenterY
+
+-- set up forward references
